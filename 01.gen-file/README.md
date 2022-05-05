d58add296f9f4379d0a9bcef7549618e630b4d53
sam-pettyjohn <sammpj47@gmail.com>
2022-05-05 16:03:50 -0400

    massive update to generating markdown template

--- 01.gen-file/README.md
+++ 01.gen-file/README.md
@@ -1,1 +1,67 @@
-`generated README file`
+![MIT](https://img.shields.io/badge/License-MIT-blue)
+# README-generator
+
+## Description
+
+Quickly create a professional README for a new project.
+
+## Table of Contents
+
+- [README-generator](#readme-generator)
+  - [Description](#description)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+## Installation
+
+Please install "Inquirer" via npm.
+
+## Usage
+
+`TBD by user`
+
+## Contributing
+
+Anyone is able to contribute.
+    
+## Tests
+
+`TBD by user`
+
+## Questions
+
+For any questions, please reach out to sam-pettyjohn through the following resources:
+
+Name: Samuel Pettyjohn <br>
+Title: Developer <br>
+Email: <sammpj@gmail.com> <br>
+GitHub: <https://github.com/sam-pettyjohn/> <br>
+
+## License
+
+
+MIT License
+
+Copyright (c) 2022 Samuel Pettyjohn
+            
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+            
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+            
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
--- index.js
+++ index.js
@@ -1,13 +1,173 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const fs = require("fs"); 
+const inquirer = require("inquirer");
+const genMD = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// questions pertaining to user:
+const aboutUser = () => {
+    console.log(`
+ [USER INPUT]   
+    `)
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter your NAME:",
+            validate: userName => {
+                if (userName) {
+                    return true;
+                } else {
+                    console.log("User's name must be provided - Please enter your name:");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "profession",
+            message: "Please enter your TITLE:",
+            validate: userProf => {
+                if (userProf) {
+                    return true;
+                } else {
+                    console.log("User's profession must be provided - Please enter your profession:");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your EMAIL address:",
+            validate: userEmail => {
+                if (userEmail) {
+                    return true;
+                } else {
+                    console.log("User's email must be provided - Please enter your email address:");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Please enter your GITHUB USERNAME:",
+            validate: userGitHub => {
+                if (userGitHub) {
+                    return true;
+                } else {
+                    console.log("User's GitHub username must be provided - Please enter your GitHub Username:");
+                    return false;
+                }
+            }
+        }
+    ]);      
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// questions pertaining to project:
+const aboutProject = inputData => {
+    console.log(`
+ [PROJECT INPUT]
+    `)
 
-// TODO: Create a function to initialize app
-function init() {}
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "license",
+            message: "Please select a LICENSE from the following list:",
+            choices: ["MIT", "APACHE 2.0", "GPL 3.0"],
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "Please enter a TITLE for your project:",
+            validate: projectTitle => {
+                if (projectTitle) {
+                    return true;
+                } else {
+                    console.log("Project must include a title - Please enter a title for your project:");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please enter a DESCRIPTION for your project:",
+            validate: projectDescription => {
+                if (projectDescription) {
+                    return true;
+                } else {
+                    console.log("Project must include a description - Please enter a description for your project:");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please enter any required INSTALLATIONS instructions for this project. If there are none, please enter 'NONE':",
+            validate: projectInstall => {
+                if (projectInstall) {
+                    return true;
+                } else {
+                    console.log("Please provide text entry. Enter any required installation instructions for this project. If there are none, please enter 'NONE':");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please provide USAGE information for the project:",
+            validate: projectUsage => {
+                if (projectUsage) {
+                    return true;
+                } else {
+                    console.log("Please provide a USAGE example for the project:");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Please include project CONTRIBUTION GUIDELINES for other users:",
+            validate: projectContribution => {
+                if (projectContribution) {
+                    return true;
+                } else {
+                    console.log("Please include project contribution guidelines for others:");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Please provide TEST INSTRUCTIONS for this project:",
+            validate: projectTests => {
+                if (projectTests) {
+                    return true;
+                } else {
+                    console.log("Please provide test instructions for this project:");
+                    return false;
+                }
+            }
+        },
+    ])
+    .then((readmeData) => {
+        const compData = Object.assign(inputData, readmeData);
+        console.log(compData);
+        return compData;
+    });
+};
 
-// Function call to initialize app
-init();
+aboutUser()
+    .then(aboutProject)
+    .then(inputData => {
+        const pushInputData = genMD(inputData);
+        fs.writeFile("./01.gen-file/README.md", pushInputData, (err) => {
+            if (err) throw new Error(err);
+        });
+    });
